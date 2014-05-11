09a044255710ad95e5259a3641162a4163368cd7
Carlton <keedyc@carleton.edu>
2014-05-10 23:43:37 -0500

    centered map to Carleton campus

---
+++ Its A Trap/IATMapViewController.h
@@ -0,0 +1,13 @@
+//
+//  IATMapViewController.h
+//  Its A Trap
+//
+//  Created by Carlton Keedy on 5/10/14.
+//  Copyright (c) 2014 Its-A-Trap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IATMapViewController : UIViewController
+
+@end
---
+++ Its A Trap/IATMapViewController.m
@@ -0,0 +1,49 @@
+//
+//  IATMapViewController.m
+//  Its A Trap
+//
+//  Created by Carlton Keedy on 5/10/14.
+//  Copyright (c) 2014 Its-A-Trap. All rights reserved.
+//
+
+#import "IATMapViewController.h"
+
+@interface IATMapViewController ()
+
+@end
+
+@implementation IATMapViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
