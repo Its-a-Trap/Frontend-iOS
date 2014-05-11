5993835c80e8f9a49ad0faf02d52c32f091e3e53
Carlton <keedyc@carleton.edu>
2014-05-11 01:53:34 -0500

    Added sweep confirmation popup.

---
+++ Its A Trap/IATSignUpViewController.h
@@ -0,0 +1,13 @@
+//
+//  IATSignUpViewController.h
+//  Its A Trap
+//
+//  Created by Carlton Keedy on 5/11/14.
+//  Copyright (c) 2014 Its-A-Trap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IATSignUpViewController : UIViewController
+
+@end
