c05f6218498e41808b74b2210c9150b7005798b4
Adam Canady <adam@adamcanady.com>
2014-05-24 02:54:17 -0500

    Fixed Facebook button locations, added log out button to slideout drawer  Please enter the commit message for your changes. Lines starting

---
+++ IATLeaderBoardViewController.h
@@ -0,0 +1,13 @@
+//
+//  IATLeaderBoardViewController.h
+//  Its A Trap
+//
+//  Created by Adam Canady on 5/24/14.
+//  Copyright (c) 2014 Its-A-Trap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IATLeaderBoardViewController : UIViewController
+
+@end
